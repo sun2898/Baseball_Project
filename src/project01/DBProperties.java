7ce30a068d1559313d378d8b96ab342ef953c5f3
sun2898 <alfo3347@cnu.ac.kr>
2023-05-30 10:02:57 +0900

    ip수정

--- src/project01/DBProperties.java
+++ src/project01/DBProperties.java
@@ -2,11 +2,11 @@ package project01;
 
 public class DBProperties {
 
-	//public static final String URL =  "jdbc:oracle:thin:@172.30.1.71:1521:xe"; //jdbc:oracle:thin:호스트이름:포트번호:DB이름
-	//public static final String UID =  "VOLLEYBALL";
+	public static final String URL =  "jdbc:oracle:thin:@172.30.1.71:1521:xe"; //jdbc:oracle:thin:호스트이름:포트번호:DB이름
+	public static final String UID =  "VOLLEYBALL";
 	
-	public static final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
-	public static final String UID = "USER01";
+//	public static final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
+//	public static final String UID = "USER01";
 	
 	public static final String UPW =  "1234";
 	
